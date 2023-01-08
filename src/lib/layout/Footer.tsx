28fbf261e0c4ce55281d8c6e32d0cbf4e96940e6
Dan Hannigan <hi@dan-h.dev>
2023-01-08 11:46:38 -0700

    feat(layout): adds netlify button to bottom

--- src/lib/layout/Footer.tsx
+++ src/lib/layout/Footer.tsx
@@ -1,8 +1,15 @@
-import { Flex, Link, Text } from "@chakra-ui/react";
+import { Flex, Link, Text, Icon } from "@chakra-ui/react";
+import { SiNetlify } from "react-icons/si";
 
 const Footer = () => {
   return (
     <Flex as="footer" width="full" justifyContent="center" paddingY="4">
+      <Text fontSize="sm" marginRight="2" color="wine.500">
+        <Link href="https://www.netlify.com" target="_blank">
+          <Icon as={SiNetlify} marginRight="1" />
+          This site is powered by Netlify
+        </Link>
+      </Text>
       <Text fontSize="sm" color="wine.500">
         {new Date().getFullYear()} -{" "}
         <Link
