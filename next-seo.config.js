7a27ce994c55fc6421a54ddb9dc0260964c4bdea
Dan Hannigan <hi@dan-h.dev>
2023-01-09 09:28:49 -0700

    fix(styles): social images fix

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next-seo').DefaultSeoProps} */
+
 const defaultSEOConfig = {
   title: "Denver Tech Social Club",
   titleTemplate: "%s | Denver Tech Social Club",
-  defaultTitle: "Denver DeTechvs Social Club",
+  defaultTitle: "Denver Tech Social Club",
   description:
     "Denver Tech Social Club is a digital & physical third place that encourages connections & friendships among the Denver tech community. Talk shop, talk hobbies, make sub-clubs (board games, anyone?), and more!",
   canonical: "https://denvertechsocial.club",
@@ -13,7 +14,7 @@ const defaultSEOConfig = {
       "Denver Tech Social Club is a digital & physical third place that encourages connections & friendships among the Denver tech community. Talk shop, talk hobbies, make sub-clubs (board games, anyone?), and more!",
     images: [
       {
-        url: "",
+        url: "DTSC-OGimg.png",
         alt: "Denver Tech Social Club",
       },
     ],
