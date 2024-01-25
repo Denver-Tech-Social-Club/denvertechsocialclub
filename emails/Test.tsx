8493f152e14dc8a4cfe81b80eec7676f8b102183
Zach G <zachjamesgreen@gmail.com>
2024-01-25 16:56:41 -0700

    edit email template

--- emails/Test.tsx
+++ emails/Test.tsx
@@ -9,12 +9,9 @@ import {
   Text,
   Body,
   Tailwind,
+  Hr,
+  Container,
 } from "@react-email/components";
-
-const bodyStyle = {
-  backgroundColor: "#ffffff",
-  margin: "0 auto",
-};
 
 interface Props {
   name: string;
@@ -23,9 +20,6 @@ interface Props {
 export default function HelloEmail({ name }: Props) {
   return (
     <Html>
-      <Head>
-        <title>Hello</title>
-      </Head>
       <Tailwind
         config={{
           theme: {
@@ -37,18 +31,37 @@ export default function HelloEmail({ name }: Props) {
           },
         }}
       >
-        <Body style={bodyStyle}>
-          <Text>
-            Hi there {name}! Thanks for showing interest in the Denver Tech
-            Social Club. You should get your invite to the discord server
-            shortly.
-          </Text>
-          <Button
-            href="https://www.meetup.com/denver-tech-social-club/"
-            className="bg-brand text-white p-2 rounded"
-          >
-            DTSC Meetup
-          </Button>
+        <Head>
+          <title>Hello</title>
+        </Head>
+
+        <Body className="bg-white flex flex-col sm:w-screen">
+          <Container>
+            <Img
+              src="http://cdn.mcauto-images-production.sendgrid.net/852303f649c594f2/3469f6ae-6d5b-494a-b2b6-ead3dda47be7/600x200.png"
+              alt="Denver Tech Social Club Header Image"
+            ></Img>
+            <Text>
+              Hi there {name}! Thanks for showing interest in the Denver Tech
+              Social Club. You should get your invite to the discord server
+              shortly.
+            </Text>
+            <Hr></Hr>
+            <div className="md:flex md:justify-center sm:block">
+              <Button
+                href="https://www.meetup.com/denver-tech-social-club/"
+                className="bg-brand text-white p-2 rounded m-2"
+              >
+                DTSC Meetup
+              </Button>
+              <Button href="#" className="bg-brand text-white p-2 rounded m-2">
+                DTSC Code of Conduct
+              </Button>
+              <Button href="#" className="bg-brand text-white p-2 rounded m-2">
+                DTSC Rules
+              </Button>
+            </div>
+          </Container>
         </Body>
       </Tailwind>
     </Html>
