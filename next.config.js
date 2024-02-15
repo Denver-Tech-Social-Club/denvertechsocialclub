675c624788c26159e0f61774999b7dd108e3a520
Zach G <zachjamesgreen@gmail.com>
2024-02-14 22:02:35 -0700

    dont min

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const withPWA = require("next-pwa")({
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
-  swcMinify: true,
+  swcMinify: false,
   reactStrictMode: true,
   eslint: {
     dirs: ["src"],
